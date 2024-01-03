5a023c60187c49718a206cd39d9ce77ce0559995
subakyat571 <dovertomaz@gmail.com>
2024-01-03 22:45:35 +0100

    Add project files.

---
+++ StopWatch/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace StopWatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            startBTN = new Button();
+            stopBTN = new Button();
+            resetBTN = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("ROG Fonts", 50F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(54, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(698, 96);
+            label1.TabIndex = 0;
+            label1.Text = "00:00:00:00";
+            label1.Click += label1_Click;
+            // 
+            // startBTN
+            // 
+            startBTN.BackColor = Color.Lime;
+            startBTN.FlatStyle = FlatStyle.Flat;
+            startBTN.Font = new Font("Segoe Print", 13.7739134F, FontStyle.Regular, GraphicsUnit.Point);
+            startBTN.ForeColor = Color.White;
+            startBTN.Location = new Point(106, 187);
+            startBTN.Name = "startBTN";
+            startBTN.Size = new Size(160, 60);
+            startBTN.TabIndex = 1;
+            startBTN.Text = "START";
+            startBTN.UseVisualStyleBackColor = false;
+            startBTN.Click += startBTN_Click;
+            // 
+            // stopBTN
+            // 
+            stopBTN.BackColor = Color.Red;
+            stopBTN.FlatStyle = FlatStyle.Flat;
+            stopBTN.Font = new Font("Segoe Print", 13.7739134F, FontStyle.Regular, GraphicsUnit.Point);
+            stopBTN.ForeColor = Color.White;
+            stopBTN.Location = new Point(307, 187);
+            stopBTN.Name = "stopBTN";
+            stopBTN.Size = new Size(160, 60);
+            stopBTN.TabIndex = 2;
+            stopBTN.Text = "STOP";
+            stopBTN.UseVisualStyleBackColor = false;
+            stopBTN.Click += stopBTN_Click;
+            // 
+            // resetBTN
+            // 
+            resetBTN.BackColor = Color.FromArgb(255, 128, 255);
+            resetBTN.FlatStyle = FlatStyle.Flat;
+            resetBTN.Font = new Font("Segoe Print", 13.7739134F, FontStyle.Regular, GraphicsUnit.Point);
+            resetBTN.ForeColor = Color.White;
+            resetBTN.Location = new Point(502, 187);
+            resetBTN.Name = "resetBTN";
+            resetBTN.Size = new Size(160, 60);
+            resetBTN.TabIndex = 3;
+            resetBTN.Text = "RESET";
+            resetBTN.UseVisualStyleBackColor = false;
+            resetBTN.Click += resetBTN_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.MidnightBlue;
+            ClientSize = new Size(786, 300);
+            Controls.Add(resetBTN);
+            Controls.Add(stopBTN);
+            Controls.Add(startBTN);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button startBTN;
+        private Button stopBTN;
+        private Button resetBTN;
+    }
+}
---
+++ StopWatch/Form1.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace StopWatch
+{
+    public partial class Form1 : Form
+    {
+
+        private System.Timers.Timer timer = new();
+        private int h, m, s, ms;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTimer();
+        }
+
+        private void InitializeTimer()
+        {
+            timer.Interval = 1;
+            timer.Elapsed += OnTimeEvent;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+            timer.Start();
+        }
+
+        private void startBTN_Click(object sender, EventArgs e)
+        {
+
+            timer.Start();
+        }
+
+        private void stopBTN_Click(object sender, EventArgs e)
+        {
+
+            timer.Stop();
+        }
+
+        private void resetBTN_Click(object sender, EventArgs e)
+        {
+
+            timer.Stop();
+            h = 0;
+            m = 0;
+            s = 0;
+            ms = 0;
+            label1.Text = "00:00:00:00";
+        }
+
+        private void OnTimeEvent(object? sender, ElapsedEventArgs e)
+        {
+
+            Invoke(new Action(() =>
+            {
+                ms += 1;
+                if (ms == 100)
+                {
+                    ms = 0;
+                    s += 1;
+                }
+                if (s == 60)
+                {
+                    s = 0;
+                    m += 1;
+                }
+                if (m == 60)
+                {
+                    m = 0;
+                    h += 1;
+                }
+
+                label1.Text = string.Format("{0}:{1}:{2}:{3}",
+                    h.ToString().PadLeft(2, '0'),
+                    m.ToString().PadLeft(2, '0'),
+                    s.ToString().PadLeft(2, '0'),
+                    ms.ToString().PadLeft(2, '0'));
+            }));
+
+            if (Console.KeyAvailable)
+            {
+                char key = Console.ReadKey(intercept: true).KeyChar;
+                if (key == 'a')
+                {
+                    timer.Stop();
+                    Application.Exit();
+                }
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
